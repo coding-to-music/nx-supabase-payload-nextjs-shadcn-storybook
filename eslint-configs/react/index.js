fd519729c227baba50abe6ccba0dbd9ae7406b24
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2024-12-05 14:48:21 +0200

    define react config in legacy format (.eslintrc)

--- eslint-configs/react/index.js
+++ eslint-configs/react/index.js
@@ -1,39 +1,18 @@
-module.exports = (async () => {
-    const standardReact = require("eslint-config-standard-react");
-    const standardReactClone = {...standardReact};
-    delete standardReactClone.parserOptions;
-    standardReactClone.languageOptions = {
-        ecmaVersion: 2021,
-        parserOptions: {
-            ecmaFeatures: {
-                jsx: true,
-            },
-        },
-        sourceType: "module",
-    };
-    standardReactClone.plugins = {
-        react: require("eslint-plugin-react"),
-        "react-hooks": require("eslint-plugin-react-hooks"),
-    };
-    return [
-        standardReactClone,
-        require("eslint-config-prettier"),
-        {
-            rules: {
-                "react/jsx-curly-brace-presence": [
-                    "warn",
-                    {props: "always", children: "never"},
-                ],
-                "react/jsx-sort-props": [
-                    "warn",
-                    {
-                        callbacksLast: true,
-                        shorthandLast: true,
-                        ignoreCase: true,
-                        reservedFirst: true,
-                    },
-                ],
-            },
-        },
-    ];
-})();
+module.exports = {
+  extends: ["standard-react", "prettier"],
+  rules: {
+      "react/jsx-curly-brace-presence": [
+          "warn",
+          {props: "always", children: "never"},
+      ],
+      "react/jsx-sort-props": [
+          "warn",
+          {
+              callbacksLast: true,
+              shorthandLast: true,
+              ignoreCase: true,
+              reservedFirst: true,
+          },
+      ],
+  },
+};

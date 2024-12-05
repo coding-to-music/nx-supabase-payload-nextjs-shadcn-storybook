3af83ae5ca894941c5e649581128d5ecfd1a7918
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2024-12-05 19:43:59 +0200

    fix scripts/typecheck-lint-and-prettier.bash

--- scripts/typecheck-lint-and-prettier.bash
+++ scripts/typecheck-lint-and-prettier.bash
@@ -3,9 +3,9 @@ set -euo pipefail
 IFS=$'\n\t'
 
 if [[ -n "$CONVENIENCE_SCRIPTS_ALL" ]]; then
-    pnpm nx run-many --targets=typecheck,lint # maximize parallelization opportunity
+    pnpm nx run-many --targets=typecheck,lint $* # maximize parallelization opportunity
     pnpm prettier:all $*
 else
-    pnpm nx affected --targets=typecheck,lint # maximize parallelization opportunity
+    pnpm nx affected --targets=typecheck,lint $* # maximize parallelization opportunity
     pnpm prettier $*
 fi

f325e9e2378921b773d2bec521a375c89ba39e2e
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2024-12-05 19:49:04 +0200

    fix eslint.config.js in react-components

--- libs/react-components/eslint.config.js
+++ libs/react-components/eslint.config.js
@@ -1,6 +1,8 @@
 import {fixupConfigRules} from "@eslint/compat";
 import {FlatCompat} from "@eslint/eslintrc";
 import nx from "@nx/eslint-plugin";
+import eslintPluginReact from "eslint-plugin-react";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
 import baseConfig from "../../eslint.config.js";
 
 const compat = new FlatCompat({
@@ -19,7 +21,17 @@ export default [
                 },
             ],
         }),
-    ),
+    )
+        // temporary workaround for https://github.com/eslint/eslintrc/issues/135
+        .map((config) => {
+            if (config.plugins?.react) {
+                config.plugins.react = eslintPluginReact;
+            }
+            if (config.plugins?.["react-hooks"]) {
+                config.plugins["react-hooks"] = eslintPluginReactHooks;
+            }
+            return config;
+        }),
     {
         files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
         // Override or add rules here

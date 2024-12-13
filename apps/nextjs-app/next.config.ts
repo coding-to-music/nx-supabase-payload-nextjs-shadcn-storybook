73ee79b99bfb7753f4fc78fb23d252c86ba3388d
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2024-12-13 09:33:09 +0200

    add images section from Payload website template to next.config.ts

--- apps/nextjs-app/next.config.ts
+++ apps/nextjs-app/next.config.ts
@@ -1,13 +1,29 @@
 //@ts-check
-/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, unicorn/prefer-module */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, unicorn/prefer-module, @typescript-eslint/prefer-nullish-coalescing, @typescript-eslint/strict-boolean-expressions */
 
 const {composePlugins, withNx} = require("@nx/next");
 const {withPayload} = require("@payloadcms/next/withPayload");
+
+const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+    images: {
+        remotePatterns: [
+            NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */,
+        ].map((item) => {
+            const url = new URL(item);
+
+            return {
+                hostname: url.hostname,
+                protocol: url.protocol.replace(":", ""),
+            };
+        }),
+    },
     nx: {
         // Set this to true if you would like to use SVGR
         // See: https://github.com/gregberge/svgr

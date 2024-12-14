a634e198aeda89f8f519f5aec06b1e6f0ead0891
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2024-12-14 10:14:01 +0200

    add blocks/

---
+++ libs/payload/src/blocks/index.ts
@@ -0,0 +1,7 @@
+export * from "./archive";
+export * from "./banner";
+export * from "./code";
+export * from "./content";
+export * from "./cta";
+export * from "./form-block";
+export * from "./media-block";

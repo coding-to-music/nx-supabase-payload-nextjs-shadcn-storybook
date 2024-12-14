bfd880f3177e82a1e8212dc443891c9ecd6dbda2
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2024-12-14 10:39:33 +0200

    do not default export deepMerge

--- libs/payload/src/utils/deep-merge.ts
+++ libs/payload/src/utils/deep-merge.ts
@@ -14,7 +14,7 @@ export function isObject(item: unknown): boolean {
  * @param target
  * @param ...sources
  */
-export default function deepMerge<T, R>(target: T, source: R): T {
+export function deepMerge<T, R>(target: T, source: R): T {
   const output = { ...target }
   if (isObject(target) && isObject(source)) {
     Object.keys(source).forEach((key) => {

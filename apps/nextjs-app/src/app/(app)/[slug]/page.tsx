a6acd1b80fdd1dae1a423eb07e8469b3d2311946
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2024-12-14 11:45:54 +0200

    move seed/ and home-static.ts closer to usage

--- apps/nextjs-app/src/app/(app)/[slug]/page.tsx
+++ apps/nextjs-app/src/app/(app)/[slug]/page.tsx
@@ -5,13 +5,13 @@ import configPromise from '@payload-config'
 import { getPayload } from 'payload'
 import { draftMode } from 'next/headers'
 import React, { cache } from 'react'
-import { homeStatic } from '@/endpoints/seed/home-static'
 
 import type { Page as PageType } from '@/payload-types'
 
 import { RenderBlocks } from '@/blocks/RenderBlocks'
 import { RenderHero } from '@/heros/RenderHero'
 import { generateMeta } from '@/utilities/generateMeta'
+import { homeStatic } from './_/home-static'
 import PageClient from './page.client'
 
 export async function generateStaticParams() {

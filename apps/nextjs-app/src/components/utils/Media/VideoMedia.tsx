c2d152b448c140e55a1af9d23da56988d23bc033
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2024-12-18 11:10:07 +0200

    fix typo in VideoMedia

--- apps/nextjs-app/src/components/utils/Media/VideoMedia.tsx
+++ apps/nextjs-app/src/components/utils/Media/VideoMedia.tsx
@@ -9,22 +9,19 @@ import type {Props as MediaProps} from "./types";
 export const VideoMedia: React.FC<MediaProps> = (props) => {
     const {onClick, resource, videoClassName} = props;
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call -- [bulk suppress]
-    const videoRef = React.seRef<HTMLVideoElement>(null);
+    const videoRef = React.useRef<HTMLVideoElement>(null);
     // const [showFallback] = useState<boolean>()
 
     React.useEffect(() => {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- [bulk suppress]
         const {current: video} = videoRef;
         // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions -- [bulk suppress]
         if (video) {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access -- [bulk suppress]
             video.addEventListener("suspend", () => {
                 // setShowFallback(true);
                 // console.warn('Video was suspended, rendering fallback image.')
             });
         }
-    }, [videoRef]);
+    }, []);
 
     // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions -- [bulk suppress]
     if (resource && typeof resource === "object") {
@@ -32,7 +29,6 @@ export const VideoMedia: React.FC<MediaProps> = (props) => {
 
         return (
             <video
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- [bulk suppress]
                 ref={videoRef}
                 className={cn(videoClassName)}
                 controls={false}

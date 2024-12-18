17e039e44783b38b04c56d07b131f29af5abd5b7
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2024-12-18 23:51:13 +0200

    configure fonts in Storybook

--- libs/react-components/.storybook/preview.tsx
+++ libs/react-components/.storybook/preview.tsx
@@ -1,8 +1,19 @@
 import type {Decorator, Parameters} from "@storybook/react";
+import localFont from "next/font/local";
 import React from "react";
 import {useDarkMode} from "storybook-dark-mode";
 
 import "./global.css";
+
+const GeistSans = localFont({
+    src: "./fonts/Geist-Variable.woff2",
+    variable: "--font-geist-sans",
+});
+
+const GeistMono = localFont({
+    src: "./fonts/GeistMono-Variable.woff2",
+    variable: "--font-geist-mono",
+});
 
 export const decorators: Decorator[] = [
     (Story) => {
@@ -15,6 +26,13 @@ export const decorators: Decorator[] = [
                 delete root.dataset.theme;
             };
         }, [darkMode]);
+        return <Story />;
+    },
+    (Story) => {
+        React.useEffect(() => {
+            const root = globalThis.document.documentElement;
+            root.classList.add(GeistSans.variable, GeistMono.variable);
+        }, []);
         return <Story />;
     },
 ];

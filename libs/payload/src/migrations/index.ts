86ae9daffc6455598b7c1f7ce1abf34094ff00f9
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2024-12-19 12:07:07 +0200

    add initial migration

---
+++ libs/payload/src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20241219_095028 from './20241219_095028';
+
+export const migrations = [
+  {
+    up: migration_20241219_095028.up,
+    down: migration_20241219_095028.down,
+    name: '20241219_095028'
+  },
+];

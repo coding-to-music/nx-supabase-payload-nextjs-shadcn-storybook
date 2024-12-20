e66ab4279a31f7b90355b66886812dfa74c28a4b
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2024-12-20 09:43:45 +0200

    format code

--- apps/nextjs-app-e2e/src/example.spec.ts
+++ apps/nextjs-app-e2e/src/example.spec.ts
@@ -4,5 +4,7 @@ test("has title", async ({page}) => {
     await page.goto("/");
 
     // Expect h1 to contain a substring.
-    expect(await page.locator("h1").textContent()).toContain("Payload Website Template");
+    expect(await page.locator("h1").textContent()).toContain(
+        "Payload Website Template",
+    );
 });

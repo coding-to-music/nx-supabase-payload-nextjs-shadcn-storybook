73f345fcb725f0404e8165e510e101e7e53e9399
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2025-01-11 20:00:02 +0200

    change styles

--- apps/nextjs-app/src/app/(app)/(auth)/sign-up/page.tsx
+++ apps/nextjs-app/src/app/(app)/(auth)/sign-up/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
     return (
         <div
             className={
-                "flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10"
+                "flex min-h-svh flex-col items-center justify-center gap-6 bg-card p-6 md:p-10"
             }
         >
             <div className={"flex w-full max-w-sm flex-col gap-6"}>

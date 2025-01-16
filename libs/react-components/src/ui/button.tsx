278fe59959098e59a3dcd7bb60d757c2fa98b49f
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2025-01-16 06:00:20 +0200

    set default type="button"

--- libs/react-components/src/ui/button.tsx
+++ libs/react-components/src/ui/button.tsx
@@ -44,10 +44,12 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     ({className, variant, size, asChild = false, ...props}, ref) => {
         const Comp = asChild ? Slot : "button";
+        const type = Comp === "button" ? "button" : undefined;
         return (
             <Comp
                 ref={ref}
                 className={cn(buttonVariants({variant, size, className}))}
+                type={type}
                 {...props}
             />
         );

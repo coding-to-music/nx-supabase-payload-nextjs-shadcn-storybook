4c01f64149da1de7be0cf85453620f448c33d786
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2025-01-16 06:03:51 +0200

    add export for Command component

--- libs/react-components/src/index.ts
+++ libs/react-components/src/index.ts
@@ -2,6 +2,7 @@
 export * from "./ui/button";
 export * from "./ui/card";
 export * from "./ui/checkbox";
+export * from "./ui/command";
 export * from "./ui/input";
 export * from "./ui/label";
 export * from "./ui/pagination";
